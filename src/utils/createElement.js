2ed27e741d535e087c874750aa20cf005b5c2e19
Lukasz Nogaj <lukasz.nogajdev@gmail.com>
2026-03-11 13:09:11 +0000

    refactor: simplify createElement helper

--- src/utils/createElement.js
+++ src/utils/createElement.js
@@ -2,9 +2,7 @@ export function createElement(tag, className, idName) {
 	const element = document.createElement(tag);
 
 	if (className) {
-		if (className) {
-			element.className = className;
-		}
+		element.className = className;
 	}
 
 	if (idName) {
